430b2a128f20c6f1a67bc623280d271bcb9d9298
Skkomolkina <skomolkina@gmail.com>
2021-10-24 14:42:12 +0300

    fixed: add promo's adaptive

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -10,12 +10,15 @@ function Promo(props) {
         <div className='promo'>
             <Header />
 
-            <div className='promo__grid'>
+            <div className='promo__flex'>
                 <img src={landingImg} className='promo__image' alt='landing-logo' />
-                <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
-                <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
 
-                <button className='promo__button'>Узнать больше</button>
+                <div className='promo__flex-text'>
+                    <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+                    <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+                    <button className='promo__button'>Узнать больше</button>
+                </div>
+
             </div>
         </div>
     );
